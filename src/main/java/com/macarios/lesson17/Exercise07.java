ee30f4c553420d7685b5bee6ab8dcd701f71d492
Victor Macarios <47126430+vmacarios@users.noreply.github.com>
2020-04-08 15:09:24 +0200

    Updated exercise 07 - Code simplified.

--- src/main/java/com/macarios/lesson17/Exercise07.java
+++ src/main/java/com/macarios/lesson17/Exercise07.java
@@ -8,21 +8,19 @@ public class Exercise07 implements Exercise {
     @Override
     public String execute() {
         Scanner scanner = new Scanner(System.in);
-        double[] nums = new double[5];
-        double max;
+        double num;
+        double max = Integer.MIN_VALUE;
         System.out.print("Insert 5 numbers: ");
         for (int i = 0; i < 5; i++) {
             while (!scanner.hasNextDouble()) {
                 scanner.next();
             }
-            nums[i] = scanner.nextDouble();
-        }
-        max = nums[0];
-        for (int i = 1; i < 5; i++) {
-            if (nums[i] > max) {
-                max = nums[i];
+            num = scanner.nextDouble();
+            if (num > max) {
+                max = num;
             }
         }
+
         return "The highest number is: " + max;
     }
 }
